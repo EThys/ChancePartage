c87939bc8e64a96f4ffa4317a2e1a4f830d6e30d
Muzola Ethberg <ethbergmuzola500@gmail.com>
2025-03-13 12:29:02 +0100

    Feat: Mise en place des migrations USER,EVENT,RESERVATION ET WINNER

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('last_name')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('profession')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->string('nationality')->nullable();
+            $table->string('current_city')->nullable();
+            $table->string('profile_photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->integer('isAdmin')->default(0);
+            $table->string('password');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
